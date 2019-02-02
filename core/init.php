7695ab4afd675ea2b589bb70b1417a0b41e17a30
venkattumu <venkyjob452@gmail.ocm>
2019-02-02 19:09:56 +0530

    complete init.php

--- core/init.php
+++ core/init.php
@@ -1,6 +1,16 @@
 <?php
-include 'core/database/connection.php';
+include 'database/connection.php';
 include 'classes/user.php';
 include 'classes/follow.php';
 include 'classes/tweet.php';
+
+global $pdo;
+
+session_start();
+
+$getFromUser = new User($pdo);
+$getFromFollow = new Follow($pdo);
+$getFromTweet = new Tweet($pdo);
+
+define("BASE_URL", "http://localhost/codingdaddy/");
 ?>
